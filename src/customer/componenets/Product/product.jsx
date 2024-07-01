94cb789a09cb76f7b005061309c48875eafdf2a9
Manoj Kumar Rajpoot <MKRP97584@GMAIL.COM>
2024-07-01 20:20:45 +0530

    added the pics to product

--- src/customer/componenets/Product/product.jsx
+++ src/customer/componenets/Product/product.jsx
@@ -17,56 +17,10 @@ import { mens_kurta } from '../../../data/mens_kurta.js'
 import Productcard from './productcard'
 
 const sortOptions = [
-  { name: 'Most Popular', href: '#', current: true },
-  { name: 'Best Rating', href: '#', current: false },
-  { name: 'Newest', href: '#', current: false },
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
-const subCategories = [
-  { name: 'Totes', href: '#' },
-  { name: 'Backpacks', href: '#' },
-  { name: 'Travel Bags', href: '#' },
-  { name: 'Hip Bags', href: '#' },
-  { name: 'Laptop Sleeves', href: '#' },
-]
-const filters = [
-  {
-    id: 'color',
-    name: 'Color',
-    options: [
-      { value: 'white', label: 'White', checked: false },
-      { value: 'beige', label: 'Beige', checked: false },
-      { value: 'blue', label: 'Blue', checked: true },
-      { value: 'brown', label: 'Brown', checked: false },
-      { value: 'green', label: 'Green', checked: false },
-      { value: 'purple', label: 'Purple', checked: false },
-    ],
-  },
-  {
-    id: 'category',
-    name: 'Category',
-    options: [
-      { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-      { value: 'sale', label: 'Sale', checked: false },
-      { value: 'travel', label: 'Travel', checked: true },
-      { value: 'organization', label: 'Organization', checked: false },
-      { value: 'accessories', label: 'Accessories', checked: false },
-    ],
-  },
-  {
-    id: 'size',
-    name: 'Size',
-    options: [
-      { value: '2l', label: '2L', checked: false },
-      { value: '6l', label: '6L', checked: false },
-      { value: '12l', label: '12L', checked: false },
-      { value: '18l', label: '18L', checked: false },
-      { value: '20l', label: '20L', checked: false },
-      { value: '40l', label: '40L', checked: true },
-    ],
-  },
-]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -163,7 +117,7 @@ export default function Product() {
         </Dialog>
 
         <main className="mx-auto px-4 sm:px-6 lg:px-20">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-8">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">New Arrivals</h1>
 
             <div className="flex items-center">
--- src/customer/componenets/Product/productcard.jsx
+++ src/customer/componenets/Product/productcard.jsx
@@ -11,14 +11,14 @@ function Productcard({product}) {
 
       <div className='textPart bg-white p-3'>
         <div>
-          <p className='font-bold opacity-60'>Universaloutfit</p>
-          <p>Casual Puff Sleeves Solid Women White Top</p>
+          <p className='font-bold opacity-60'>{product.brand}</p>
+          <p>{product.title}</p>
         </div>
 
         <div className='flex items-center space-x-2'>
-          <p className='font-semibold'>199</p>
-          <p className='line-through opacity-50'>*1999</p>
-          <p className=' text-green-600 font-semibold'>70% off</p>
+          <p className='font-semibold'>₹{product.discountedPrice}</p>
+          <p className='line-through opacity-50'>{product.price}</p>
+          <p className=' text-green-600 font-semibold'>{product.discountPersent}% off</p>
         </div>
 
       </div>
